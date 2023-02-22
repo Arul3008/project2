18a7767aaa84c682e50acc989faa3163367128d9
Arul3008 <arulk3777@gmail.com>
2023-02-22 11:03:02 -0800

    new code added

---
+++ src/org/computer/Computer.java
@@ -0,0 +1,14 @@
+package org.computer;
+
+public class Computer  {
+	public void computerModel() {
+		// TODO Auto-generated method stub
+		System.out.println("HP model");
+
+	}
+	public static void main(String[] args) {
+		Computer c = new Computer();
+		c.computerModel();
+	}
+
+}
---
+++ src/org/computer/Desktop.java
@@ -0,0 +1,14 @@
+package org.computer;
+
+public class Desktop extends Computer {
+	private void desktopSize() {
+		// TODO Auto-generated method stub
+         System.out.println("32 bit");
+	}
+public static void main(String[] args) {
+	Desktop d = new Desktop();
+	d.desktopSize();
+	d.computerModel();
+	
+}
+}
